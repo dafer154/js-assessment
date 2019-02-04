91efc51b6f4bc5d1ffd621c3bfe533da700a85f5
dafer154 <david_fza@hotmail.com>
2019-02-03 22:11:33 -0500

    Implement reduceString method with regex

--- app/strings.js
+++ app/strings.js
@@ -9,7 +9,17 @@ stringsAnswers = {
    * @param {Number} amount - The maximum number of adjacent repeated letters in the result string.
    * @returns {String} A string with no more than amount number of repeated letters.
    */
-  reduceString: function reduceString(str, amount) {},
+  reduceString: function reduceString(str, amount) {
+    let i = 0;
+    while (i < str.length) {
+      const currentChart = str.charAt(i);
+      const maxLengString = currentChart.repeat(amount);
+      str = str.replace(new RegExp(`[${currentChart}]{${amount + 1},}`, 'g'), maxLengString);
+      i += 1;
+    }
+
+    return str;
+  },
 
   /**
    * Reverses a string of text
@@ -20,10 +30,10 @@ stringsAnswers = {
    * @returns {String} The original string of text str reversed.
    */
   reverseString: function reverseString(str) {
-    var result = "";
-    for (var i = str.length - 1; i >= 0; i--) {
+    let result = '';
+    for (let i = str.length - 1; i >= 0; i -= 1) {
       result += str[i];
     }
     return result;
-  }
+  },
 };
